564b5823d026c2d74ea94f3b07569ad2956a8918
austinmdx <austinmaddox@maddoxbox.com>
2023-06-13 11:40:24 -0400

    Fixed issues where elements are not displayed sometimes and undefined error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,23 @@ window.Webflow ||= [];
 window.Webflow.push(greetUser);
 
 window.fsAttributes = window.fsAttributes || [];
+
 window.fsAttributes.push([
   'cmsfilter',
   cmsFilter
 ])
+
+window.fsAttributes.push([
+  'cmsload',
+  (listInstances) => {
+    console.log('cmsload Successfully loaded!');
+
+    // The callback passes a `listInstances` array with all the `CMSList` instances on the page.
+    const [listInstance] = listInstances;
+
+    // The `renderitems` event runs whenever the list renders items after switching pages.
+    listInstance.on('renderitems', (renderedItems) => {
+      console.log('RenderedItem:', renderedItems);
+    });
+  },
+]);
--- src/utils/articles.js
+++ src/utils/articles.js
@@ -10,9 +10,9 @@ const createItem = (article, templateElement) => {
   const contentType = newItem.querySelector('[data-element="content-type"]');
 
   // Populate inner elements
-  if (image) image.src = article.featuredImage.url;
+  if (image) image.src = article?.featuredImage?.url || '';
   if (title) title.textContent = article.title;
-  if (role) role.textContent = article._role.role;
+  if (role) role.textContent = article?._role?.role;
   if (description) description.textContent = article.articleDescription;
   if (contentType) {
     contentType.textContent = article.content_type;
@@ -26,6 +26,9 @@ export const showArticleList = async (listInstance, articles) => {
   // Save a copy of the template
   const [firstItem] = listInstance.items;
   const itemTemplateElement = firstItem.element;
+  itemTemplateElement.style.opacity = 1;
+  itemTemplateElement.style.transform = "";
+  itemTemplateElement.style.transformStyle = "";
 
   // Remove existing items
   listInstance.clearItems();
